b6d8d29d6a48e9375db4827400f161dace684135
zhou <3081291537@qq.com>
2021-11-01 22:14:14 +0800

    Initial commit

---
+++ src/test/java/com/example/yeb/YebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
