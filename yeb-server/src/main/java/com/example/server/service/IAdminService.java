d6c5fc1d05af79b2661a487b8d58af78fc5884d8
30812 <3081291537@qq.com>
2021-11-05 10:20:48 +0800

    11.5

--- yeb-server/src/main/java/com/example/server/service/IAdminService.java
+++ yeb-server/src/main/java/com/example/server/service/IAdminService.java
@@ -2,6 +2,7 @@ package com.example.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.server.pojo.Admin;
+import com.example.server.pojo.RespBean;
 
 /**
  * <p>
@@ -12,5 +13,16 @@ import com.example.server.pojo.Admin;
  * @since 2021-11-04
  */
 public interface IAdminService extends IService<Admin> {
-
+    /**
+     * 登录返回token
+     * @param username
+     * @param password
+     * @return
+     */
+    RespBean login(String username, String password);
+    /**
+     * 根据用户名获取用户
+     * @param username
+     */
+    Admin getAdminByUserName(String username);
 }
--- yeb-server/src/main/java/com/example/server/service/impl/AdminServiceImpl.java
+++ yeb-server/src/main/java/com/example/server/service/impl/AdminServiceImpl.java
@@ -1,10 +1,23 @@
 package com.example.server.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.server.config.security.JwtTokenUtil;
 import com.example.server.mapper.AdminMapper;
 import com.example.server.pojo.Admin;
+import com.example.server.pojo.RespBean;
 import com.example.server.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -15,6 +28,55 @@ import org.springframework.stereotype.Service;
  * @since 2021-11-04
  */
 @Service
-public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements IAdminService {
-
+public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements
+        IAdminService {
+    @Autowired
+    private AdminMapper adminMapper;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+    /**
+     * 登录返回token
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    @Override
+    public RespBean login(String username, String password) {
+        UserDetails userDetails =
+                userDetailsService.loadUserByUsername(username);
+        if (null == userDetails || !passwordEncoder.matches(password,
+                userDetails.getPassword())) {
+            return RespBean.error("用户名或密码不正确!");
+        }
+        if (!userDetails.isEnabled()){
+            return RespBean.error("账号被禁用，请联系管理员!");
+        }
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null,
+                        userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtTokenUtil.generateToken(userDetails);
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", token);
+        tokenMap.put("tokenHead", tokenHead);
+        return RespBean.success("登录成功", tokenMap);
+    }
+    /**
+     * 根据用户名获取用户
+     *
+     * @param username
+     * @return
+     */
+    @Override
+    public Admin getAdminByUserName(String username) {
+        return adminMapper.selectOne(new QueryWrapper<Admin>().eq("username",
+                username));
+    }
 }
